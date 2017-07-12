c088eafc7294ba6577458b4a3fba5e462be3e0a5
Miroslav Tynovsky <tynovsky@avast.com>
2017-07-12 11:44:12 +0200

    implement get_dependencies

--- cpanfile
+++ cpanfile
@@ -1,6 +1,14 @@
-requires 'perl', '5.008001';
+requires 'perl', '5.012';
+
+requires 'XML::LibXML';
+requires 'Class::Tiny';
+
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
 };
 
+on 'develop' => sub {
+    requires 'Minilla';
+    requires 'Module::Build::Tiny';
+};
--- lib/Nuspec/Reader.pm
+++ lib/Nuspec/Reader.pm
@@ -1,10 +1,49 @@
 package Nuspec::Reader;
-use 5.008001;
+
+our $VERSION = '0.1.0';
+
+use 5.012;
 use strict;
 use warnings;
+use Class::Tiny qw( nuspec_filename );
+use XML::LibXML;
+use XML::LibXML::XPathContext;
 
-our $VERSION = "0.01";
+sub get_dependencies {
+    my ($self) = @_;
 
+    my $dom = XML::LibXML->load_xml(location => $self->nuspec_filename);
+    my $xpath_context = XML::LibXML::XPathContext->new($dom->documentElement);
+    $xpath_context->registerNs(
+        ns => 'http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd'
+    );
+    my $dependency_xpath =
+        '/ns:package' .
+        '/ns:metadata' .
+        '/ns:dependencies' .
+        '/ns:dependency';
+
+    return [
+        map {
+            {
+                id      => $_->findvalue('@id'),
+                version => $_->findvalue('@version'),
+            }
+        } $xpath_context->findnodes($dependency_xpath)
+    ]
+}
+
+sub get_files {
+    ...
+}
+
+sub get_version {
+    ...
+}
+
+sub get_package_id {
+    ...
+}
 
 
 1;
@@ -14,7 +53,7 @@ __END__
 
 =head1 NAME
 
-Nuspec::Reader - It's new $module
+Nuspec::Reader - Parse .nuspec file and get access to different parts.
 
 =head1 SYNOPSIS
 
@@ -26,7 +65,7 @@ Nuspec::Reader is ...
 
 =head1 LICENSE
 
-Copyright (C) Miroslav Tynovsky.
+Copyright (C) Avast Software
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
